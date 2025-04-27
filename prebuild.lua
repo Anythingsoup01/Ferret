93edf0853c9289b82cdd47ddc5efc518be80d3a2
Anythingsoup01 <williamenglish1220@gmail.com>
2025-04-27 16:30:17 -0400

    updated prebuild.lua

---
+++ prebuild.lua
@@ -0,0 +1,9 @@
+workspace "Ferret-Framework"
+    architecture "x64"
+
+external "Ferret"
+external "vendor/glad"
+external "vendor/glfw"
+external "vendor/imgui"
+external "vendor/spdlog"
+external "vendor/stb"
