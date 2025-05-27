fd1f5f684950612115bc92d535ab4078ab4df58e
Anythingsoup01 <williamenglish1220@gmail.com>
2025-05-27 16:55:23 -0400

    Added a way to set up default ini files for ImGui

--- prebuild.lua
+++
@@ -1,9 +0,0 @@
-workspace "Ferret-Framework"
-    architecture "x64"
-
-external "Ferret"
-external "vendor/glad"
-external "vendor/glfw"
-external "vendor/imgui"
-external "vendor/spdlog"
-external "vendor/stb"
